604f821b1510f93a4e67f4b70669b80b01771bd4
владимир <sitnik792@gmail.ru>
2025-10-28 22:32:07 +0300

    hw.18

--- src/org/skypro/skyshop/searching/SearchEngine.java
+++ src/org/skypro/skyshop/searching/SearchEngine.java
@@ -2,21 +2,22 @@ package org.skypro.skyshop.searching;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeMap;
 
 public class SearchEngine {
     private final List<Searchable> data = new ArrayList<>();
 
-    // Добавляем объект в поисковый движок
+    // Добавляет объект в поисковую базу
     public void add(Searchable item) {
         data.add(item);
     }
 
-    // Поиск подходящих результатов
-    public List<Searchable> search(String query) {
-        List<Searchable> results = new ArrayList<>();
+    // Поиск подходящих результатов и возврат отсортированной мапы
+    public TreeMap<String, Searchable> search(String query) {
+        TreeMap<String, Searchable> results = new TreeMap<>();
         for (Searchable item : data) {
             if (item.getSearchTerm().contains(query)) {
-                results.add(item);
+                results.put(item.getName(), item); // Заполняем отсортированную мапу
             }
         }
         return results;
